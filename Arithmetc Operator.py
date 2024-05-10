7d050f63426f0d0fbb62c5f53e56e47e5aa1f62d
Bikila Tariku <135463709+bikilas@users.noreply.github.com>
2024-05-10 15:31:08 +0300

    Create Arithmetc Operator.py

---
+++ Arithmetc Operator.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Calculate sum
+    print(a + b)
+    
+    # Calculate difference
+    print(a - b)
+    
+    # Calculate product
+    print(a * b)
