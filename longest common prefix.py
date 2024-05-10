df0d21443b95a0674be492cd37e4989f06cf9738
Bikila Tariku <135463709+bikilas@users.noreply.github.com>
2024-05-10 16:08:27 +0300

    Update longest common prefix.py

--- longest common prefix.py
+++ longest common prefix.py
@@ -1,1 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        
+        # Iterate through characters of the first string
+        for i, char in enumerate(strs[0]):
+            # Check if the character is present in the same position in all other strings
+            for string in strs[1:]:
+                if i >= len(string) or string[i] != char:
+                    return strs[0][:i]
+        
+        return strs[0]
+
+
 
